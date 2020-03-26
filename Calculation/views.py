2cf9170a0104334ee997917306583f8891b7c369
nnadlin <53036225+nnadlin@users.noreply.github.com>
2020-03-26 11:07:30 +0800

    Calculation process

---
+++ Calculation/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+#Create your views here
+
+def home(request):
+    return render(request,'home.html',{'name':'Nnadlin'})
+
+def add(request):
+
+    value1 = int(request.POST['num1'])
+    value2 = int(request.POST['num2'])
+    result = value1 + value2
+
+    return  render(request,'result.html', {'result':result})
